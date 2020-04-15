bf50cb65e6a547889f765e5449512f42640adc2e
Henry Izontimi <henperi@Henrys-MacBook-Pro.local>
2020-04-15 20:26:41 +0100

    change to cloud api

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
-  apiUrl: 'http://localhost:4212/farm4me-484cb/us-central1/cloud/api/v1/',
-  // apiUrl: 'https://us-central1-farm4me-484cb.cloudfunctions.net/cloud/api/v1/',
+  // apiUrl: 'http://localhost:4212/farm4me-484cb/us-central1/cloud/api/v1/',
+  apiUrl: 'https://us-central1-farm4me-484cb.cloudfunctions.net/cloud/api/v1/',
   payStack: {
     pubKey: 'pk_test_7f68d92bac4b100e5c029fa098aeefca7babce85',
   },
